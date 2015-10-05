42616d3282c3a17b9e1bcd8c33ec8ba255ff070b
Josh83 <Josh@CrashPunk.com>
2015-10-04 21:52:29 -0700

    More messing

--- sketcher.js
+++ sketcher.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
 	
-	var gridRows = 30;	
-	var gridColumns = 25;
+	var gridColumns = 32;
 	var counter = 0;
 	var margin = 1;
 	var initial_dot_color = "#E6E6E6";
 	
 	var wrapperWidth = $(".bubble_wrapper").width();
+	var wrapperHeight = $(".bubble_wrapper").height();
 	var divSize = Math.floor((wrapperWidth-2.1*(gridColumns))/gridColumns);
 	var divRadius = Math.floor(divSize / 2);
+	
+	 var gridRows = Math.floor(wrapperHeight / divSize);
 	
 	for (var i = 0; i< gridRows; i++) {
 		for ( var j= 0; j < gridColumns; j++) {
@@ -16,6 +18,12 @@ $(document).ready(function() {
 		}
 		$(".bubble_wrapper").append("<br>");
 	};
+	
+	$(function() {
+		$("#btnClear").click(function(){
+			$(".color_block").css("background-color", initial_dot_color);
+		});
+	});
 	
 	$(".color_block").mouseenter(function() {
 		$(this).css("background-color", getColor(counter++));
