a828d6306c27bd64dc38413206f9b89c73858f58
Josh83 <Josh@CrashPunk.com>
2015-10-04 17:34:23 -0700

    Full page
    Updated so that the dots would automatically size themselves based on A)
    how many rows/columns I've specified in code, and B) how big the browser
    window is. Also did a few other things.

--- sketcher.js
+++ sketcher.js
@@ -1,32 +1,33 @@
 $(document).ready(function() {
 	
-	var gridSize = 25;
-	var baseColor =  "#E6E6E6";
-	
+	var gridRows = 30;	
+	var gridColumns = 25;
 	var counter = 0;
+	var margin = 1;
+	var initial_dot_color = "#E6E6E6";
 	
-	for (var i = 0; i< gridSize; i++) {
-		for ( var j= 0; j < gridSize; j++) {
-			$(".wrapper").append("<div class='color_block'></div>");
+	var wrapperWidth = $(".bubble_wrapper").width();
+	var divSize = Math.floor((wrapperWidth-2.1*(gridColumns))/gridColumns);
+	var divRadius = Math.floor(divSize / 2);
+	
+	for (var i = 0; i< gridRows; i++) {
+		for ( var j= 0; j < gridColumns; j++) {
+			$(".bubble_wrapper").append("<div class='color_block' style='height:" + divSize + "px; width:"  + divSize + "px; border-radius:" + divRadius + "px; margin:" + margin + "px; background-color:" + initial_dot_color + "'></div>");
 		}
-		$(".wrapper").append("<br>");
+		$(".bubble_wrapper").append("<br>");
 	};
 	
 	$(".color_block").mouseenter(function() {
 		$(this).css("background-color", getColor(counter++));
 	});
 	
-/*  	$(".color_block").mouseleave(function() {
-		$(this).css("background-color", baseColor);
-	});  */
-	
+		
 	var getColor = function(counterNumber) {
+		counterNumber *= 25;
 		var r = ((counterNumber>>8) & 0xf) * 0x11;
 		var g = ((counterNumber>>4) & 0xf) * 0x11;
 		var b = (counterNumber & 0xf) * 0x11;
 		return("rgb(" + r + "," + g +"," + b +")");
 	};
-	
-	
 	
 });
